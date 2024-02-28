e72cf1ab78cc1c7012dc58a4542ecd48a950a466
Matt Harlow <mharlow85@gmail.com>
2024-02-28 07:41:11 -0800

    Adds WorkoutForm component

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,5 +1,18 @@
+import React, { useState } from 'react';
+import WorkoutForm from './WorkoutForm';
+import { useAppContext } from './AppContext';
+
+// Page for new workout
+// Allow you to pick exercise, then listing entry boxes for each set, reps, and weight
 const Workout = () => {
-    return
+    const { workoutId, workoutDate, workoutForm, addWorkoutForm } = useAppContext();
+
+    return (
+        <>
+            <h2>New Workout</h2>
+            <WorkoutForm key={workoutForm.formId} />
+        </>
+    )
 }
 
 export default Workout;
