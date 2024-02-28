4132cdf836249162267bbcc3ce9042543fb10e72
Matt Harlow <mharlow85@gmail.com>
2024-02-28 08:58:34 -0800

    Final server changes

--- server.js
+++ server.js
@@ -9,19 +9,19 @@ import workoutExerciseRouter from './routes/workoutExerciseRouter.js';
 
 dotenv.config();
 
-const pgConnect = `postgres://mharlow9:3l5kE8Jb0vSPPeWFYANqyQM1oJEZldeq@dpg-cnef407109ks738v8h30-a/harlowdb`
-const pgConnectLocal = `postgres://postgres:postgres@localhost:6432/postgres`
+// const pgConnect = `postgres://mharlow9:3l5kE8Jb0vSPPeWFYANqyQM1oJEZldeq@dpg-cnef407109ks738v8h30-a/harlowdb`
+// const pgConnectLocal = `postgres://postgres:postgres@localhost:6432/postgres`
 const { Pool } = pkg;
-const app = express();
-const port = 10000;
-
+let PORT = process.env.PORT || 10000;
 
 const pool = new Pool({
-    connectionString: pgConnect,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false, // Only use this in development
     }
 });
+
+const app = express();
 
 // Middleware
 app.use(
@@ -41,6 +41,6 @@ app.use('/api/workouts', workoutRouter);
 app.use('/api/exercises', exerciseRouter);
 app.use('/api/workout-details', workoutExerciseRouter);
 
-app.listen(port, () => {
-    console.log("Listening on port: ", port);
+app.listen(PORT, () => {
+    console.log("Listening on port: ", PORT);
 });
