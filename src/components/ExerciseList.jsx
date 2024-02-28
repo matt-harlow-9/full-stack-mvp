0e9e7ee290212d6f46551054ce77afb0ac98c35e
Matt Harlow <mharlow85@gmail.com>
2024-02-28 07:38:29 -0800

    Returns list of exercises

--- src/components/ExerciseList.jsx
+++ src/components/ExerciseList.jsx
@@ -1,15 +1,29 @@
-import SingleExercise from "./SingleExercise";
+import React, { useEffect } from 'react';
+import { useAppContext } from './AppContext';
 
-const ExerciseList = (props) => {
-    
-    const handleClick = () => {
-        props.getExerciseList();
-    }
-    const exerciseList = props.exerciseList;
-    return exerciseList.map((exercise) => (
-        <SingleExercise name={exercise.name} category={exercise.category} description={exercise.description} />
-    ))
-
+const ExerciseList = () => {
+    const { navigateToPage, exerciseNameList, setExerciseNameList } = useAppContext();
+    useEffect(() => {
+        fetch('http://localhost:3000/api/exercises')
+            .then((res) => res.json())
+            .then((data) => {
+                const names = data.map((exercise) => exercise.name);
+                setExerciseNameList(names);
+            })
+            .catch((error) => console.error('Error fetching exercise name list: ', error));
+    }, []);
+    return (
+        <div>
+            <h1>Exercise List</h1>
+            <ul>
+                {exerciseNameList.map((exName, index) => (
+                    <li key={index}>
+                        <button onClick={() => navigateToPage('exerciseDetails', index + 1)}>{exName}</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default ExerciseList
