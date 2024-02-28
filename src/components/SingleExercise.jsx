d38b504f74b009d1d282440663eec1074fff390c
Matt Harlow <mharlow85@gmail.com>
2024-02-28 07:40:48 -0800

    Adds single exercise component

--- src/components/SingleExercise.jsx
+++ src/components/SingleExercise.jsx
@@ -1,6 +1,16 @@
 const SingleExercise = (props) => {
     
-    return
+    const handleClick = () => {
+        setCurrent
+    }
+
+    return (
+        <div>
+            <div>{props.name}</div>
+            <div>Category: {props.category}</div>
+            <div>Description: {props.description}</div>
+        </div>
+    )
 }
 
 export default SingleExercise;
