0f5c6a5ff73acb41a401b71cf3db3206562c57f5
Matt Harlow <mharlow85@gmail.com>
2024-02-28 07:38:12 -0800

    Adds functionality to exercise details comp to display details of an exercise

---
+++ src/components/ExerciseDetails.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { useAppContext } from './AppContext';
+
+const ExerciseDetails = () => {
+    const {
+        navigateToPage,
+        selectedExerciseId,
+        selectedExerciseDetails,
+        setSelectedExerciseDetails
+    } = useAppContext();
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/api/exercises/${selectedExerciseId}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setSelectedExerciseDetails(data[0]);
+            })
+            .catch((error) => console.error('Error fetching exercise details: ', error))
+    }, []);
+
+    return (
+        <div>
+            <h2>{selectedExerciseDetails.name}</h2>
+            <p>Category: {selectedExerciseDetails.category}</p>
+            <p>Description: {selectedExerciseDetails.description}</p>
+            <button onClick={() => navigateToPage('exerciseList')}>Back To Exercises</button>
+        </div>
+    )
+}
+
+export default ExerciseDetails;
